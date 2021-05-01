3e3f50b3b061ea23b03ae5e0a32ef174f2773cdb
Mediogre <caseyarrington13@gmail.com>
2021-04-30 19:55:20 -0500

    add Express with listener/routes for html

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const router = require('express').Router();
+
+// Home Page Route
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+// Notes HTML Page Route
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+
+module.exports = router;
+
+
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+// Router/HTML Paths
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// -------------------------
+// Express Vars & Middleware
+// -------------------------
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// HTML Imported Routes/Router
+app.use('/', htmlRoutes);
+app.use(express.static('public'));
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+})
+
